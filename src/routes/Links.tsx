13fe7c34145e952ddcfef68aaecca451a3431f29
Rterego <Rteregoshirokuro@gmail.com>
2023-01-24 21:23:54 +0900

    feat: Linksページの作成

--- src/routes/Links.tsx
+++ src/routes/Links.tsx
@@ -4,14 +4,27 @@ import { COMMON_CONTENTS_STYLE } from "../foundations/styles";
 const common = COMMON_CONTENTS_STYLE;
 
 const useStyles = makeStyles({
+    container: {
+        position: 'relative',
+        maxWidth: '100%',
+        alignItems: 'center',
+        top: 64
+    },
+    text: {
+        fontSize: 24
+    }
 });
 
-const Links = () =>{
+const Links = () => {
     const commonClasses = common();
-    return(
+    const classes = useStyles();
+
+    return (
         <div className={commonClasses.contents}>
             <h1 className={commonClasses.title}>LINKS</h1>
-            
+            <div className={classes.container}>
+                <p className={classes.text}>GitHub: <a href='https://github.com/Rterego'>https://github.com/Rterego</a></p>
+            </div>
         </div>
     )
 }
